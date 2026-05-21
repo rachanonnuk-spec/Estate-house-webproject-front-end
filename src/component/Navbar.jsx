8ad81994c5555bfe52d4a8c380761f5ac9f937b4
rachanonsrichai <rachanon.nuk@gmail.com>
2026-05-21 11:21:16 +0700

    added sign up path

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -24,14 +24,18 @@ const Navbar = () => {
         <div className='container mx-auto max-w-7xl flex justify-between items-center py-4 px-6 md:px-20 lg:px-32  bg-transparent'>
             <img src={assets.logo} alt="" />
             <ul className='hidden md:flex gap-7 text-white'>
-                <a href="#Header" className='cursor-pointer hover:text-grey-400'>Home</a>    
-                <a href="#About" className='cursor-pointer hover:text-grey-400'>About</a>
-                <a href="#Projects" className='cursor-pointer hover:text-grey-400'>Project</a>
-                <a href="#Testimonials" className='cursor-pointer hover:text-grey-400'>Testimonials</a>
+                <a href="#Header" className='cursor-pointer hover:text-grey-400 hover:underline'>Home</a>    
+                <a href="#About" className='cursor-pointer hover:text-grey-400 hover:underline'>About</a>
+                <a href="#Projects" className='cursor-pointer hover:text-grey-400 hover:underline'>Project</a>
+                <a href="#Testimonials" className='cursor-pointer hover:text-grey-400 hover:underline'>Testimonials</a>
             </ul>
-          <button className='hidden md:block bg-white px-8 py-2 rounded-xl'>Sign Up</button>
+          {/* <button href="#Contact" className='hidden md:block bg-white px-8 py-2 rounded-xl hover:bg-blue-400'>Sign Up</button> */}
+          <div className=''>
+        <a href="#Contact" className= 'hidden md:block bg-white px-8 py-2 rounded-xl hover:bg-blue-400 hover:bg-blue-400'>Sign Up</a>
+          </div>
           <img onClick={()=> setShowMobileMenu(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt=""/>
         </div>
+
         {/* ----mobile-menu---- */}
         <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0' } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
           <div className='flex items-center justify-end p-6 cursor-pointer'>
